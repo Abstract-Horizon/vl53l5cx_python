219be5959ac288cfc8cddea0af68429d2d1079ca
Daniel Sendula <natdan@users.noreply.github.com>
2022-02-28 17:12:35 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="VL53L5CX",
+    version="1.0.0",
+    author="Daniel Sendula",
+    description="Python native implementation of VL53L5CX ultra light driver code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Abstract-Horizon/vl53l5cx_python",
+    packages=['vl53l5cx'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
